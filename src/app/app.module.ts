c640e41f4203a3fd6180747da6ac334c0ef7a769
Rabindranath Ferreira <ferreiralox@gmail.com>
2019-01-03 00:56:46 -0300

    add 2 components for using ng-model and databinding for Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// componentes
+import { AngFormsComponent } from './components/ang-forms/ang-forms.component';
+import { DataComponent } from './components/data/data.component';
+import { TemplateComponent } from './components/template/template.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngFormsComponent,
+    DataComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/ang-forms/ang-forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ang-forms',
+  templateUrl: './ang-forms.component.html',
+  styleUrls: ['./ang-forms.component.css']
+})
+export class AngFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
